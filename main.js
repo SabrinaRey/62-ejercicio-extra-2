e5c2bf8dd96558d7629572262cf3a3234d69574d
Malena Rey <malerey@male.5d640511d691.sn.mynetname.net>
2020-01-15 22:21:07 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const imagesAsStrings = [
+  "https://www.w3schools.com/css/img_forest.jpg",
+  "https://www.w3schools.com/css/img_5terre.jpg",
+  "https://www.w3schools.com/css/img_forest.jpg",
+  "https://www.w3schools.com/css/img_lights.jpg",
+  "https://www.w3schools.com/css/img_mountains.jpg"
+]
+
+const imagesAsObjects = [
+  {
+    title: "Forest",
+    img: "https://www.w3schools.com/css/img_forest.jpg"
+  },
+  {
+    title: "5 terre",
+    img: "https://www.w3schools.com/css/img_5terre.jpg"
+  },
+  {
+    title: "Northern lights",
+    img: "https://www.w3schools.com/css/img_lights.jpg"
+  },
+  {
+    title: "Montains",
+    img: "https://www.w3schools.com/css/img_mountains.jpg"
+  }
+]
+  
+  
+  
+  
+  
